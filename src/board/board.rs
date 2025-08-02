0445d49ed9966df1624ae6ecfa51d66a9769f096
Gjones747 <gsman707@gmail.com>
2025-08-02 15:07:21 -0700

    quickly writting out board checking logic

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use crate::board;
+
 
 pub struct Board {
     board_array: [[Tile; 9]; 9], // defines the layout of all the tiles 
@@ -42,7 +46,7 @@ impl Board {
             locked: false, 
             position: [-1, -1],
             val: 0,
-            possible_values: [0; 9]
+            //possible_values: [0; 9]
         };
 
         let mut board = Board {
@@ -72,11 +76,69 @@ impl Board {
     }
 
     // functions associated with board 
-    fn find_possible_values(&self, tile: &mut Tile) {
-        println!("{0}", tile.position[0]);
-    }
+    // fn find_possible_values(&self, tile: &mut Tile) {
+    //     println!("{0}", tile.position[0]);
+    // }
 
-    fn check_solution(&self) {
+    fn check_board(&self) {
+        fn check_row(board: Board, row_number: i8) -> bool {
+            let mut previous: HashMap<i8, i8>= HashMap::new();
+
+            for i in 0..9 {
+                let current_square:Tile = board.board_array[row_number as usize][i];
+                if previous.contains_key(&current_square.val){
+                    return false
+                }
+                previous.insert(current_square.val as i8, i as i8);
+
+            }
+
+            return true
+
+        }
+
+        fn check_col(board: Board, col_number: i8) -> bool {
+            let mut previous: HashMap<i8, i8>= HashMap::new();
+
+            for i in 0..9 {
+                let current_square:Tile = board.board_array[i][col_number as usize];
+                if previous.contains_key(&current_square.val){
+                    return false
+                }
+                previous.insert(current_square.val as i8, i as i8);
+
+            }
+
+            return true
+        }
+
+        fn check_box(board: Board, row: i8, col:i8) -> bool {
+
+            fn individual_box(board:Board, start_row:i8, start_col:i8) -> bool {
+                let mut previous: HashMap<i8, i8>= HashMap::new();
+                for row in start_row..(start_row+3) {
+                    for col in start_col..(start_col+3) {
+                        let current_tile:Tile = board.board_array[row as usize][col as usize];
+                        if previous.contains_key(&current_tile.val){
+                            return false
+                        }
+                        previous.insert(current_tile.val as i8, row as i8);
+
+                    }
+                }
+
+                return true
+            }
+
+            if row/3 >= 2 &&  col/3 >= 2 {
+                return individual_box(board, 6, 6)
+            } else if row/3 >= 2 && col/3 >= 1 {
+                return individual_box(board, 6, 3);
+            }
+
+            return true
+        }
+
 
     }
 }
@@ -85,7 +147,7 @@ impl Board {
 struct Tile {
     val: i8, // 0 if null
     locked: bool,
-    possible_values: [i8; 9],
+    // possible_values: [i8; 9],
     position: [i8; 2] // each tile will store its own position in the board ROW COL
 }
 
